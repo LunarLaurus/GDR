e54c803c0d99c355c7617a0374c7290f043104a9
LunarLaurus <draknyte1@gmail.com>
2026-02-24 07:33:03 +0000

    feat: complete Phase 2 - increase MAXVISPLANES from 128 to 256

--- src/doom/r_plane.c
+++ src/doom/r_plane.c
@@ -42,7 +42,7 @@ planefunction_t		ceilingfunc;
 //
 
 // Here comes the obnoxious "visplane".
-#define MAXVISPLANES	128
+#define MAXVISPLANES	256
 visplane_t		visplanes[MAXVISPLANES];
 visplane_t*		lastvisplane;
 visplane_t*		floorplane;
